b3f6b3b4fb8c66dbf1806a240860b6bd845957cd
Antonio Carlos do Nascimento Junior <jr_acn@hotmail.com>
2015-10-14 22:39:45 -0300

    Adicionando Projeto.

---
+++ MetricaEngenhariaSoftware.Core/Entidade/TabelaDominioContainer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MetricaEngenhariaSoftware.Core.Entidade
+{
+    public class TabelaDominioContainer
+    {
+        public List<TabelaDominio> TabelaDominio { get; set; }
+        public TabelaDominio Geral  => new TabelaDominio
+        {
+            NomeTabela = "Geral",
+            QuantidadeAtributos = TabelaDominio.Select(x=> x.QuantidadeAtributos).Sum()
+        };
+    }
+}
---
+++ MetricaEngenhariaSoftware.Core/Entidade/TabelaEntrada.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace MetricaEngenhariaSoftware.Core.Entidade
+{
+    public class ListaTabelaEntrada
+    {
+        public List<TabelaEntrada> TabelaEntrada { get; set; }
+    }
+
+    public class TabelaEntrada
+    {
+        public int NumeroOcorrencia { get; set; }
+        public TabelaEntradaPeso Complexidade { get; set; }
+        public int Peso => (int)Complexidade;
+        public int Resultado => NumeroOcorrencia * Peso;
+    }
+
+    public enum TabelaEntradaPeso
+    {
+        Simples = 3,
+        Medio = 4,
+        Complexo = 6,
+    }
+}
