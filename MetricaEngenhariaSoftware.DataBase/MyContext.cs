052df16d541ea493eb9228170b51fa315d07bca5
Antonio Carlos do Nascimento Junior <jr_acn@hotmail.com>
2015-11-02 18:07:42 -0200

    Configurando webconfig

--- MetricaEngenhariaSoftware.DataBase/MyContext.cs
+++ MetricaEngenhariaSoftware.DataBase/MyContext.cs
@@ -1,22 +1,22 @@
-﻿using System.Data.Common;
-using System.Data.Entity;
-using MetricaEngenhariaSoftware.Entity.Entidade;
+﻿using System.Data.Entity;
 using MetricaEngenhariaSoftware.Entity.Entidade.MES;
 
 namespace MetricaEngenhariaSoftware.DataBase
 {
     public class MyContext : DbContext
     {
-        public MyContext() : base("name=AAA")
+        public MyContext() : base("name=ConectionDB")
         {
-            
+
         }
 
         protected void OnModelCreating()
         {
-            
         }
 
+        public virtual DbSet<MES_FA> MES_FA { get; set; }
+        public virtual DbSet<MES_ISO> MES_ISO { get; set; }
+        public virtual DbSet<MES_LINGUAGEM_PROGRAMACAO> MES_LINGUAGEM_PROGRAMACAO { get; set; }
         public virtual DbSet<MES_TIPO_SISTEMA> MES_TIPO_SISTEMA { get; set; }
     }
 
