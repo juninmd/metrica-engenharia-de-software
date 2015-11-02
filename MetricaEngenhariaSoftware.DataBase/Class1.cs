0ebce7000365a43248150e8aeba4406daa50c38e
Antonio Carlos do Nascimento Junior <jr_acn@hotmail.com>
2015-11-02 16:41:56 -0200

    Adicionando Banco

---
+++ MetricaEngenhariaSoftware.DataBase/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricaEngenhariaSoftware.DataBase
+{
+    public class Class1
+    {
+    }
+}
