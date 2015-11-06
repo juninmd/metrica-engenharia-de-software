a35b175b05e3e793d2a95a897e7d6a70afe1f35a
Antonio Carlos do Nascimento Junior <jr_acn@hotmail.com>
2015-11-06 20:27:16 -0200

    Implementando DropDown

---
+++ MetricaEngenhariaSoftware/Infra/HtmlHelpers/BaseHtmlHelpers.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace MetricaEngenhariaSoftware.Infra.HtmlHelpers
+{
+
+    public static class BaseHtmlHelper
+    {
+        public static TagBuilder DropDownList(string id, string name, string css, bool required = true, bool selecione = true)
+        {
+            var dropDown = new TagBuilder("select");
+
+            if (selecione)
+                dropDown.InnerHtml = "<option value=''>Selecione...</option>";
+
+            dropDown.Attributes.Add("id", id);
+            dropDown.Attributes.Add("name", name);
+            if (required)
+                dropDown.Attributes.Add("required", "required");
+            return dropDown;
+        }
+
+        public static TagBuilder TextBox(string id, string name, string cssClass, bool required, string personalizado, string onchange = "")
+        {
+            var txt = new TagBuilder("input");
+            txt.Attributes.Add("id", id);
+            txt.Attributes.Add("name", name);
+            txt.Attributes.Add("class", cssClass);
+            txt.Attributes.Add(personalizado, personalizado);
+            txt.Attributes.Add("onchange", onchange);
+            txt.Attributes.Add("autocomplete", "off");
+            if (required)
+                txt.Attributes.Add("required", "required");
+            return txt;
+        }
+
+        public static TagBuilder Option(string value, string text, bool selected)
+        {
+            var option = new TagBuilder("option");
+            option.Attributes.Add("value", value);
+            option.SetInnerText(text);
+            if (selected)
+                option.Attributes.Add("selected", "selected");
+            return option;
+        }
+
+        public static string DropDownListOptions<T>(IEnumerable<T> source, string text, string value, object selectedValue)
+        {
+            var enumerable = source as T[] ?? source.ToArray();
+            var sb = new StringBuilder(enumerable.Length * 20);
+
+            foreach (var item in new SelectList(enumerable, text, value, selectedValue).Where(item => !string.IsNullOrEmpty(item.Text)))
+                sb.Append(Option(item.Value, item.Text, item.Selected));
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ MetricaEngenhariaSoftware/Infra/HtmlHelpers/DropDown.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using MetricaEngenhariaSoftware.DataBase.Repository;
+using MetricaEngenhariaSoftware.Entity.Entidade.MES;
+
+namespace MetricaEngenhariaSoftware.Infra.HtmlHelpers
+{
+    public static class DropDown
+    {
+        public static MvcHtmlString TipoSistemas(this HtmlHelper h)
+        {
+            var dropDown = BaseHtmlHelper.DropDownList("IntIdTipoSistema", "IntIdTipoSistema", "", true);
+            dropDown.InnerHtml += BaseHtmlHelper.DropDownListOptions(new GenericRepository<MES_TIPO_SISTEMA>().GetAll().ToList(), "IntIdTipoSistema", "StrNomeTipoSistema", null);
+            return new MvcHtmlString(dropDown.ToString());
+        }
+
+        public static MvcHtmlString Linguagens(this HtmlHelper h)
+        {
+            var dropDown = BaseHtmlHelper.DropDownList("IntIdLinguagemProgramacao", "IntIdLinguagemProgramacao", "", true);
+            dropDown.InnerHtml += BaseHtmlHelper.DropDownListOptions(new GenericRepository<MES_LINGUAGEM_PROGRAMACAO>().GetAll().ToList(), "IntIdLinguagemProgramacao", "StrNomeLinguagemProgramacao", null);
+            return new MvcHtmlString(dropDown.ToString());
+        }
+        public static MvcHtmlString Isos(this HtmlHelper h)
+        {
+            var dropDown = BaseHtmlHelper.DropDownList("IntIdIso", "IntIdIso", "", true);
+            dropDown.InnerHtml += BaseHtmlHelper.DropDownListOptions(new GenericRepository<MES_ISO>().GetAll().ToList(), "IntIdIso", "StrNomeIso", null);
+            return new MvcHtmlString(dropDown.ToString());
+        }
+        public static MvcHtmlString Fa(this HtmlHelper h)
+        {
+            var dropDown = BaseHtmlHelper.DropDownList("IntIdFa", "IntIdFa", "", true);
+            dropDown.InnerHtml += BaseHtmlHelper.DropDownListOptions(new GenericRepository<MES_FA>().GetAll().ToList(), "IntIdFa", "DecValorFa", null);
+            return new MvcHtmlString(dropDown.ToString());
+        }
+    }
+
+}
+
